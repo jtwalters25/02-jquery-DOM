97f317a6e395c401c6451f5947b37bfd49f007c8
Jeremiah <jeremiaht.walters@gmail.com>
2017-01-03 14:10:48 -0800

    first todos done

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,19 +1,29 @@
 var articles = [];
 
 function Article (options) {
-/* TODO: This is our Model constructor. It will take in
-   our source data from blogArticles and instantiate a
-   new Object according to this new definition. options is
-   a placeholder for the object that will ultimately be
-   passed in. Use all of the properties in blogArticles
-   to populate the new Article data that we'll use.  */
-   this.title = options.title;
-};
+  /* DONE: This is our Model constructor. It will take in
+  our source data from blogArticles and instantiate a
+  new Object according to this new definition. options is
+  a placeholder for the object that will ultimately be
+  passed in. Use all of the properties in blogArticles
+  to populate the new Article data that we'll use.  */
+  this.title = options.title;
+  this.category = options.category;
+  this.author = options.author;
+  this.authorUrl = options.authorUrl;
+  this.publishedOn = options.publishedOn;
+  this.body = options.body;
+}
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
-  /* TODO: We also need to fill in:
+  $newArticle.attr('data-author', this.author);
+  $newArticle.attr('data-authorUrl', this.authorUrl);
+  $newArticle.attr('data-title', this.title);
+  $newArticle.attr('data-body', this.body);
+  $newArticle.attr('data-publishedOn', this.publishedOn);
+  /* DONE: We also need to fill in:
   1. author name
   2. author url
   3. article title
@@ -21,17 +31,18 @@ Article.prototype.toHtml = function() {
   5. publication*/
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-/* TODO: This cloned article is no longer a template, as it now
-has real data attached to it. Remove the class from this new article! */
+  /* TODO: This cloned article is no longer a template, as it now
+  has real data attached to it. Remove class from this new article! */
+  // $newArticle.removeClass('template');
   return $newArticle;
 };
 
 /* This sort method is a standard JavaScript Array function
-   that will iterate over an array and compare its values,
-   and then arrange them in ascending or descending order
-   according to the return value. We are comparing the
-   publishedOn properties to arrange the blog posts in
-   descending order (most recent first). */
+that will iterate over an array and compare its values,
+and then arrange them in ascending or descending order
+according to the return value. We are comparing the
+publishedOn properties to arrange the blog posts in
+descending order (most recent first). */
 blogArticles.sort(function(currentObject, nextObject) {
   return (new Date(nextObject.publishedOn)) - (new Date(currentObject.publishedOn));
 });
